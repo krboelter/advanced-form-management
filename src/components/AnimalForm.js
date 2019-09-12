a40ce7561e1f0b0c370e8cedf697dc76d4100a7d
krboelter <krboelter@gmail.com>
2019-09-11 19:00:02 -0600

    installed yup and added some more fields

--- src/components/AnimalForm.js
+++ src/components/AnimalForm.js
@@ -2,22 +2,41 @@ import React from 'react'
 import { withFormik, Form, Field } from 'formik';
 
 const AnimalForm = (props) => {
-  console.log(props)
-
+  
   return (
     <Form>
       <Field type="text" name="species" placeholder="Species" />
-      <Field type="text" name="age" placeholder="Age" />
+      <Field type="number" name="age" placeholder="Age" />
+
+      <Field component="select" name="diet">
+        <option value="" disabled>Select Diet Class:</option>
+        <option value="carnivore">Carnivore</option>
+        <option value="herbavore">Herbavore</option>
+        <option value="omnivore">Omnivore</option>
+      </Field>
+
+      <label>
+        <Field type="checkbox" name="vaccinations" />
+        <span>Vaccinations</span>
+      </label>
+
+      <Field component="textarea" name="notes" placeholder="Notes" />
+
       <button type="submit">Submit</button>
     </Form>
   )
 }
 
 export default withFormik({
+  // these are the values but how formik handles them
   mapPropsToValues: (values) => {
     return {
+      // this makes the inputs (value) "controlled", they line up with the "name" attributes on our Fields
       species: values.species || '', //always pass through something, even an empty string
-      age: values.age || ''
+      age: values.age || '',
+      diet: values.diet || '',
+      vaccinations: values.vaccinations || false,
+      notes: values.notes || ''
     }
   },
   handleSubmit: (values) => {
